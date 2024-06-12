87dbccbe7af0cedb85036149233b0d5b645800df
MelanieCoentreiras <melanie.coentreiras@gmail.com>
2024-06-12 11:17:40 +0200

    Alice Carousel instal

---
+++ src/components/marketing/pageSections/Events.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from "../../elements/Container";
+
+export default function Events() {
+  return (
+    <Container>
+      <div className='bg-primary shadow-lg mb-20 h-96 rounded-2xl'></div>
+    </Container>
+  );
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import { Router } from "react-router-dom";
 import HeroTop from "../components/marketing/pageSections/HeroTop";
 import LastProducts from "../components/marketing/pageSections/LastProducts";
 import Reinsurance from "../components/marketing/pageSections/Reinsurance";
+import Events from "../components/marketing/pageSections/Events";
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
       <HeroTop />
       <LastProducts />
       <Reinsurance />
+      <Events />
     </>
   );
 }
